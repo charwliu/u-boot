3321295452ccb683723124e26524f6dc67fa9554
charwliu <charwliu@ngboss.com>
2023-04-13 08:08:34 +0000

    cleanup

--- include/configs/nanopi6.h
+++ include/configs/nanopi6.h
@@ -10,44 +10,13 @@
 #ifndef __CONFIG_NANOPI6_H__
 #define __CONFIG_NANOPI6_H__
 
-#include <configs/rk3588_common.h>
-
-/* Remove or override few declarations from rk3588-common.h */
-#undef CONFIG_BOOTCOMMAND
-#undef CONFIG_DISPLAY_BOARDINFO_LATE
-#undef RKIMG_DET_BOOTDEV
-#undef RKIMG_BOOTCOMMAND
-#undef CONFIG_SYS_MMC_MAX_BLK_COUNT
-
-#define CONFIG_SYS_MMC_ENV_DEV		0
-#define CONFIG_SYS_MMC_MAX_BLK_COUNT	32768
 
 #define CONFIG_SERIAL_TAG
-
-#ifndef CONFIG_SPL_BUILD
 
 #define ROCKCHIP_DEVICE_SETTINGS \
 	"stdout=serial,vidconsole\0" \
 	"stderr=serial,vidconsole\0"
 
-#define RKIMG_DET_BOOTDEV \
-	"rkimg_bootdev=" \
-	"if mmc dev 1 && rkimgtest mmc 1; then " \
-		"setenv devtype mmc; setenv devnum 1; echo Boot from SDcard;" \
-	"elif mmc dev 0; then " \
-		"setenv devtype mmc; setenv devnum 0;" \
-	"elif rksfc dev 1; then " \
-		"setenv devtype spinor; setenv devnum 1;" \
-	"fi; \0"
-
-#define RKIMG_BOOTCOMMAND \
-	"boot_fit;" \
-	"boot_android ${devtype} ${devnum};" \
-	"bootrkp;" \
-	"run distro_bootcmd;"
-
-#define CONFIG_BOOTCOMMAND		RKIMG_BOOTCOMMAND
-
-#endif
+#include <configs/rk3588_common.h>
 
 #endif /* __CONFIG_NANOPI6_H__ */
