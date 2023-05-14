ca57519e991681cf4c059696125791cc608ca6b1
charwliu <charwliu@ngboss.com>
2023-05-14 13:20:38 +0800

    board: nanopi6 fix dwc3 usb

--- board/friendlyelec/nanopi6-rk3588/board.c
+++ board/friendlyelec/nanopi6-rk3588/board.c
@@ -29,7 +29,7 @@ static struct dwc3_device dwc3_device_data = {
 	.dr_mode = USB_DR_MODE_PERIPHERAL,
 	.index = 0,
 	.dis_u2_susphy_quirk = 1,
-	.usb2_phyif_utmi_width = 16,
+	.hsphy_mode = USBPHY_INTERFACE_MODE_UTMIW,
 };
 
 int usb_gadget_handle_interrupts(void)
