38b00eca4521e130c1c3354008d59dca7de62d9f
charwliu <charwliu@ngboss.com>
2023-05-16 10:49:42 +0800

    phy: rockchip: add rk3588 support

--- drivers/phy/rockchip/phy-rockchip-naneng-combphy.c
+++ drivers/phy/rockchip/phy-rockchip-naneng-combphy.c
@@ -4,7 +4,7 @@
  *
  * Copyright (C) 2021 Rockchip Electronics Co., Ltd.
  */
-
+#define DEBUG
 #include <common.h>
 #include <clk.h>
 #include <dm.h>
@@ -18,7 +18,70 @@
 #include <reset-uclass.h>
 #include <dm/device_compat.h>
 
-#define BIT_WRITEABLE_SHIFT		16
+#define BIT_WRITEABLE_SHIFT             16
+#define REF_CLOCK_24MHz                 (24 * HZ_PER_MHZ)
+#define REF_CLOCK_25MHz                 (25 * HZ_PER_MHZ)
+#define REF_CLOCK_100MHz                (100 * HZ_PER_MHZ)
+
+/* COMBO PHY REG */
+#define PHYREG6                         0x14
+#define PHYREG6_PLL_DIV_MASK            GENMASK(7, 6)
+#define PHYREG6_PLL_DIV_SHIFT           6
+#define PHYREG6_PLL_DIV_2               1
+
+#define PHYREG7                         0x18
+#define PHYREG7_TX_RTERM_MASK           GENMASK(7, 4)
+#define PHYREG7_TX_RTERM_SHIFT          4
+#define PHYREG7_TX_RTERM_50OHM          8
+#define PHYREG7_RX_RTERM_MASK           GENMASK(3, 0)
+#define PHYREG7_RX_RTERM_SHIFT          0
+#define PHYREG7_RX_RTERM_44OHM          15
+
+#define PHYREG8                         0x1C
+#define PHYREG8_SSC_EN                  BIT(4)
+
+#define PHYREG11                        0x28
+#define PHYREG11_SU_TRIM_0_7            0xF0
+
+#define PHYREG12                        0x2C
+#define PHYREG12_PLL_LPF_ADJ_VALUE      4
+
+#define PHYREG13                        0x30
+#define PHYREG13_RESISTER_MASK          GENMASK(5, 4)
+#define PHYREG13_RESISTER_SHIFT         0x4
+#define PHYREG13_RESISTER_HIGH_Z        3
+#define PHYREG13_CKRCV_AMP0             BIT(7)
+
+#define PHYREG14                        0x34
+#define PHYREG14_CKRCV_AMP1             BIT(0)
+
+#define PHYREG15                        0x38
+#define PHYREG15_CTLE_EN                BIT(0)
+#define PHYREG15_SSC_CNT_MASK           GENMASK(7, 6)
+#define PHYREG15_SSC_CNT_SHIFT          6
+#define PHYREG15_SSC_CNT_VALUE          1
+
+#define PHYREG16                        0x3C
+#define PHYREG16_SSC_CNT_VALUE          0x5f
+
+#define PHYREG18                        0x44
+#define PHYREG18_PLL_LOOP               0x32
+
+#define PHYREG27                        0x6C
+#define PHYREG27_RX_TRIM_RK3588         0x4C
+
+#define PHYREG32                        0x7C
+#define PHYREG32_SSC_MASK               GENMASK(7, 4)
+#define PHYREG32_SSC_DIR_SHIFT          4
+#define PHYREG32_SSC_UPWARD             0
+#define PHYREG32_SSC_DOWNWARD           1
+#define PHYREG32_SSC_OFFSET_SHIFT       6
+#define PHYREG32_SSC_OFFSET_500PPM      1
+
+#define PHYREG33                        0x80
+#define PHYREG33_PLL_KVCO_MASK          GENMASK(4, 2)
+#define PHYREG33_PLL_KVCO_SHIFT         2
+#define PHYREG33_PLL_KVCO_VALUE         2
 
 struct rockchip_combphy_priv;
 
@@ -61,8 +124,8 @@ struct rockchip_combphy_grfcfg {
 	struct combphy_reg pipe_con1_for_sata;
 	struct combphy_reg pipe_sgmii_mac_sel;
 	struct combphy_reg pipe_xpcs_phy_ready;
-	struct combphy_reg u3otg0_port_en;
-	struct combphy_reg u3otg1_port_en;
+	struct combphy_reg pipe_pcie1l0_sel;
+	struct combphy_reg pipe_pcie1l1_sel;
 };
 
 struct rockchip_combphy_cfg {
@@ -77,12 +140,22 @@ struct rockchip_combphy_priv {
 	struct regmap *pipe_grf;
 	struct regmap *phy_grf;
 	struct phy *phy;
-	struct reset_ctl_bulk phy_rsts;
+	struct reset_ctl phy_rst;
 	struct clk ref_clk;
 	const struct rockchip_combphy_cfg *cfg;
 };
 
-static int param_write(struct regmap *base,
+static void rockchip_combphy_updatel(struct rockchip_combphy_priv *priv,
+                                     int mask, int val, int reg)
+{
+        unsigned int temp;
+
+        temp = readl(priv->mmio + reg);
+        temp = (temp & ~(mask)) | val;
+        writel(temp, priv->mmio + reg);
+}
+
+static int rockchip_combphy_param_write(struct regmap *base,
 		       const struct combphy_reg *reg, bool en)
 {
 	u32 val, mask, tmp;
@@ -190,7 +263,7 @@ static int rockchip_combphy_init(struct phy *phy)
 	if (ret)
 		goto err_clk;
 
-	reset_deassert_bulk(&priv->phy_rsts);
+	reset_deassert(&priv->phy_rst);
 
 	return 0;
 
@@ -205,7 +278,7 @@ static int rockchip_combphy_exit(struct phy *phy)
 	struct rockchip_combphy_priv *priv = dev_get_priv(phy->dev);
 
 	clk_disable(&priv->ref_clk);
-	reset_assert_bulk(&priv->phy_rsts);
+	reset_assert(&priv->phy_rst);
 
 	return 0;
 }
@@ -235,10 +308,11 @@ static int rockchip_combphy_parse_dt(struct udevice *dev,
 {
 	struct udevice *syscon;
 	int ret;
+	u32 vals[4];
 
 	ret = uclass_get_device_by_phandle(UCLASS_SYSCON, dev, "rockchip,pipe-grf", &syscon);
 	if (ret) {
-		dev_err(dev, "failed to find peri_ctrl pipe-grf regmap");
+		dev_err(dev, "failed to find peri_ctrl pipe-grf regmap ret= %d\n", ret);
 		return ret;
 	}
 	priv->pipe_grf = syscon_get_regmap(syscon);
@@ -256,11 +330,16 @@ static int rockchip_combphy_parse_dt(struct udevice *dev,
 		return PTR_ERR(&priv->ref_clk);
 	}
 
-	ret = reset_get_bulk(dev, &priv->phy_rsts);
+	ret = reset_get_by_name(dev, "combphy", &priv->phy_rst);
 	if (ret) {
 		dev_err(dev, "no phy reset control specified\n");
 		return ret;
 	}
+
+	if (!dev_read_u32_array(dev, "rockchip,pcie1ln-sel-bits",
+				vals, ARRAY_SIZE(vals)))
+		regmap_write(priv->pipe_grf, vals[0],
+			     (GENMASK(vals[2], vals[1]) << 16) | vals[3]);
 
 	return 0;
 }
@@ -295,89 +374,95 @@ static int rk3568_combphy_cfg(struct rockchip_combphy_priv *priv)
 	switch (priv->mode) {
 	case PHY_TYPE_PCIE:
 		/* Set SSC downward spread spectrum */
-		val = readl(priv->mmio + (0x1f << 2));
-		val &= ~GENMASK(5, 4);
-		val |= 0x01 << 4;
-		writel(val, priv->mmio + 0x7c);
+		rockchip_combphy_updatel(priv, PHYREG32_SSC_MASK,
+					 PHYREG32_SSC_DOWNWARD << PHYREG32_SSC_DIR_SHIFT,
+					 PHYREG32);
 
-		param_write(priv->phy_grf, &cfg->con0_for_pcie, true);
-		param_write(priv->phy_grf, &cfg->con1_for_pcie, true);
-		param_write(priv->phy_grf, &cfg->con2_for_pcie, true);
-		param_write(priv->phy_grf, &cfg->con3_for_pcie, true);
+		rockchip_combphy_param_write(priv->phy_grf, &cfg->con0_for_pcie, true);
+		rockchip_combphy_param_write(priv->phy_grf, &cfg->con1_for_pcie, true);
+		rockchip_combphy_param_write(priv->phy_grf, &cfg->con2_for_pcie, true);
+		rockchip_combphy_param_write(priv->phy_grf, &cfg->con3_for_pcie, true);
 		break;
 	case PHY_TYPE_USB3:
 		/* Set SSC downward spread spectrum */
-		val = readl(priv->mmio + (0x1f << 2));
-		val &= ~GENMASK(5, 4);
-		val |= 0x01 << 4;
-		writel(val, priv->mmio + 0x7c);
+		rockchip_combphy_updatel(priv, PHYREG32_SSC_MASK,
+                                         PHYREG32_SSC_DOWNWARD << PHYREG32_SSC_DIR_SHIFT,
+                                         PHYREG32);
 
 		/* Enable adaptive CTLE for USB3.0 Rx */
-		val = readl(priv->mmio + (0x0e << 2));
-		val &= ~GENMASK(0, 0);
-		val |= 0x01;
-		writel(val, priv->mmio + (0x0e << 2));
+		val = readl(priv->mmio + PHYREG15);
+		val |= PHYREG15_CTLE_EN;
+		writel(val, priv->mmio + PHYREG15);
 
 		/* Set PLL KVCO fine tuning signals */
-		val = readl(priv->mmio + (0x20 << 2));
-		val &= ~(0x7 << 2);
-		val |= 0x2 << 2;
-		writel(val, priv->mmio + (0x20 << 2));
+		rockchip_combphy_updatel(priv, PHYREG33_PLL_KVCO_MASK,
+                                         PHYREG33_PLL_KVCO_VALUE << PHYREG33_PLL_KVCO_SHIFT,
+                                         PHYREG33);
 
 		/* Set PLL LPF R1 to su_trim[10:7]=1001 */
-		writel(0x4, priv->mmio + (0xb << 2));
+		writel(PHYREG12_PLL_LPF_ADJ_VALUE, priv->mmio + PHYREG12);
 
 		/* Set PLL input clock divider 1/2 */
-		val = readl(priv->mmio + (0x5 << 2));
-		val &= ~(0x3 << 6);
-		val |= 0x1 << 6;
-		writel(val, priv->mmio + (0x5 << 2));
+		rockchip_combphy_updatel(priv, PHYREG6_PLL_DIV_MASK,
+                                         PHYREG6_PLL_DIV_2 << PHYREG6_PLL_DIV_SHIFT,
+                                         PHYREG6);
 
 		/* Set PLL loop divider */
-		writel(0x32, priv->mmio + (0x11 << 2));
+		writel(PHYREG18_PLL_LOOP, priv->mmio + PHYREG18);
 
 		/* Set PLL KVCO to min and set PLL charge pump current to max */
-		writel(0xf0, priv->mmio + (0xa << 2));
+		writel(PHYREG11_SU_TRIM_0_7, priv->mmio + PHYREG11);
 
-		param_write(priv->phy_grf, &cfg->pipe_sel_usb, true);
-		param_write(priv->phy_grf, &cfg->pipe_txcomp_sel, false);
-		param_write(priv->phy_grf, &cfg->pipe_txelec_sel, false);
-		param_write(priv->phy_grf, &cfg->usb_mode_set, true);
+		rockchip_combphy_param_write(priv->phy_grf, &cfg->pipe_sel_usb, true);
+		rockchip_combphy_param_write(priv->phy_grf, &cfg->pipe_txcomp_sel, false);
+		rockchip_combphy_param_write(priv->phy_grf, &cfg->pipe_txelec_sel, false);
+		rockchip_combphy_param_write(priv->phy_grf, &cfg->usb_mode_set, true);
 		break;
 	case PHY_TYPE_SATA:
-		writel(0x41, priv->mmio + 0x38);
-		writel(0x8F, priv->mmio + 0x18);
-		param_write(priv->phy_grf, &cfg->con0_for_sata, true);
-		param_write(priv->phy_grf, &cfg->con1_for_sata, true);
-		param_write(priv->phy_grf, &cfg->con2_for_sata, true);
-		param_write(priv->phy_grf, &cfg->con3_for_sata, true);
-		param_write(priv->pipe_grf, &cfg->pipe_con0_for_sata, true);
+		/* Enable adaptive CTLE for SATA Rx. */
+		val = readl(priv->mmio + PHYREG15);
+		val |= PHYREG15_CTLE_EN;
+		writel(val, priv->mmio + PHYREG15);
+
+		/*
+		 * Set tx_rterm=50ohm and rx_rterm=44ohm for SATA.
+		 * 0: 60ohm, 8: 50ohm 15: 44ohm (by step abort 1ohm)
+		 */
+		val = PHYREG7_TX_RTERM_50OHM << PHYREG7_TX_RTERM_SHIFT;
+		val |= PHYREG7_RX_RTERM_44OHM << PHYREG7_RX_RTERM_SHIFT;
+		writel(val, priv->mmio + PHYREG7);
+
+		rockchip_combphy_param_write(priv->phy_grf, &cfg->con0_for_sata, true);
+		rockchip_combphy_param_write(priv->phy_grf, &cfg->con1_for_sata, true);
+		rockchip_combphy_param_write(priv->phy_grf, &cfg->con2_for_sata, true);
+		rockchip_combphy_param_write(priv->phy_grf, &cfg->con3_for_sata, true);
+		rockchip_combphy_param_write(priv->pipe_grf, &cfg->pipe_con0_for_sata, true);
 		break;
 	case PHY_TYPE_SGMII:
-		param_write(priv->pipe_grf, &cfg->pipe_xpcs_phy_ready, true);
-		param_write(priv->phy_grf, &cfg->pipe_phymode_sel, true);
-		param_write(priv->phy_grf, &cfg->pipe_sel_qsgmii, true);
-		param_write(priv->phy_grf, &cfg->sgmii_mode_set, true);
+		rockchip_combphy_param_write(priv->pipe_grf, &cfg->pipe_xpcs_phy_ready, true);
+		rockchip_combphy_param_write(priv->phy_grf, &cfg->pipe_phymode_sel, true);
+		rockchip_combphy_param_write(priv->phy_grf, &cfg->pipe_sel_qsgmii, true);
+		rockchip_combphy_param_write(priv->phy_grf, &cfg->sgmii_mode_set, true);
 		break;
 	case PHY_TYPE_QSGMII:
-		param_write(priv->pipe_grf, &cfg->pipe_xpcs_phy_ready, true);
-		param_write(priv->phy_grf, &cfg->pipe_phymode_sel, true);
-		param_write(priv->phy_grf, &cfg->pipe_rate_sel, true);
-		param_write(priv->phy_grf, &cfg->pipe_sel_qsgmii, true);
-		param_write(priv->phy_grf, &cfg->qsgmii_mode_set, true);
+		rockchip_combphy_param_write(priv->pipe_grf, &cfg->pipe_xpcs_phy_ready, true);
+		rockchip_combphy_param_write(priv->phy_grf, &cfg->pipe_phymode_sel, true);
+		rockchip_combphy_param_write(priv->phy_grf, &cfg->pipe_rate_sel, true);
+		rockchip_combphy_param_write(priv->phy_grf, &cfg->pipe_sel_qsgmii, true);
+		rockchip_combphy_param_write(priv->phy_grf, &cfg->qsgmii_mode_set, true);
 		break;
 	default:
-		pr_err("%s, phy-type %d\n", __func__, priv->mode);
+		pr_err("%s, incompatible PHY type %d\n", __func__, priv->mode);
 		return -EINVAL;
 	}
 
 	/* The default ref clock is 25Mhz */
-	param_write(priv->phy_grf, &cfg->pipe_clk_25m, true);
+	rockchip_combphy_param_write(priv->phy_grf, &cfg->pipe_clk_25m, true);
 
 	if (dev_read_bool(priv->dev, "rockchip,enable-ssc")) {
-		val = readl(priv->mmio + (0x7 << 2));
-		val |= BIT(4);
-		writel(val, priv->mmio + (0x7 << 2));
+		val = readl(priv->mmio + PHYREG8);
+		val |= PHYREG8_SSC_EN;
+		writel(val, priv->mmio + PHYREG8);
 	}
 
 	return 0;
@@ -415,8 +500,6 @@ static const struct rockchip_combphy_grfcfg rk3568_combphy_grfcfgs = {
 	.pipe_con0_for_sata	= { 0x0000, 15, 0, 0x00, 0x2220 },
 	.pipe_sgmii_mac_sel	= { 0x0040, 1, 1, 0x00, 0x01 },
 	.pipe_xpcs_phy_ready	= { 0x0040, 2, 2, 0x00, 0x01 },
-	.u3otg0_port_en		= { 0x0104, 15, 0, 0x0181, 0x1100 },
-	.u3otg1_port_en		= { 0x0144, 15, 0, 0x0181, 0x1100 },
 };
 
 static const struct rockchip_combphy_cfg rk3568_combphy_cfgs = {
@@ -431,23 +514,64 @@ static int rk3588_combphy_cfg(struct rockchip_combphy_priv *priv)
 
 	switch (priv->mode) {
 	case PHY_TYPE_PCIE:
-		param_write(priv->phy_grf, &cfg->con0_for_pcie, true);
-		param_write(priv->phy_grf, &cfg->con1_for_pcie, true);
-		param_write(priv->phy_grf, &cfg->con2_for_pcie, true);
-		param_write(priv->phy_grf, &cfg->con3_for_pcie, true);
+		rockchip_combphy_param_write(priv->phy_grf, &cfg->con0_for_pcie, true);
+		rockchip_combphy_param_write(priv->phy_grf, &cfg->con1_for_pcie, true);
+		rockchip_combphy_param_write(priv->phy_grf, &cfg->con2_for_pcie, true);
+		rockchip_combphy_param_write(priv->phy_grf, &cfg->con3_for_pcie, true);
+		rockchip_combphy_param_write(priv->pipe_grf, &cfg->pipe_pcie1l0_sel, true);
+                rockchip_combphy_param_write(priv->pipe_grf, &cfg->pipe_pcie1l1_sel, true);
 		break;
 	case PHY_TYPE_USB3:
-		param_write(priv->phy_grf, &cfg->pipe_txcomp_sel, false);
-		param_write(priv->phy_grf, &cfg->pipe_txelec_sel, false);
-		param_write(priv->phy_grf, &cfg->usb_mode_set, true);
+		/* Set SSC downward spread spectrum */
+		rockchip_combphy_updatel(priv, PHYREG32_SSC_MASK,
+					 PHYREG32_SSC_DOWNWARD << PHYREG32_SSC_DIR_SHIFT,
+					 PHYREG32);
+
+		/* Enable adaptive CTLE for USB3.0 Rx */
+		val = readl(priv->mmio + PHYREG15);
+		val |= PHYREG15_CTLE_EN;
+		writel(val, priv->mmio + PHYREG15);
+
+		/* Set PLL KVCO fine tuning signals */
+		rockchip_combphy_updatel(priv, PHYREG33_PLL_KVCO_MASK,
+					 PHYREG33_PLL_KVCO_VALUE << PHYREG33_PLL_KVCO_SHIFT,
+					 PHYREG33);
+
+		/* Set PLL LPF R1 to su_trim[10:7]=1001 */
+		writel(PHYREG12_PLL_LPF_ADJ_VALUE, priv->mmio + PHYREG12);
+
+		/* Set PLL input clock divider 1/2 */
+		rockchip_combphy_updatel(priv, PHYREG6_PLL_DIV_MASK,
+					 PHYREG6_PLL_DIV_2 << PHYREG6_PLL_DIV_SHIFT,
+					 PHYREG6);
+
+		/* Set PLL loop divider */
+		writel(PHYREG18_PLL_LOOP, priv->mmio + PHYREG18);
+
+		/* Set PLL KVCO to min and set PLL charge pump current to max */
+		writel(PHYREG11_SU_TRIM_0_7, priv->mmio + PHYREG11);
+
+		rockchip_combphy_param_write(priv->phy_grf, &cfg->pipe_txcomp_sel, false);
+		rockchip_combphy_param_write(priv->phy_grf, &cfg->pipe_txelec_sel, false);
+		rockchip_combphy_param_write(priv->phy_grf, &cfg->usb_mode_set, true);
 		break;
 	case PHY_TYPE_SATA:
-		param_write(priv->phy_grf, &cfg->con0_for_sata, true);
-		param_write(priv->phy_grf, &cfg->con1_for_sata, true);
-		param_write(priv->phy_grf, &cfg->con2_for_sata, true);
-		param_write(priv->phy_grf, &cfg->con3_for_sata, true);
-		param_write(priv->pipe_grf, &cfg->pipe_con0_for_sata, true);
-		param_write(priv->pipe_grf, &cfg->pipe_con1_for_sata, true);
+		/* Enable adaptive CTLE for SATA Rx */
+                val = readl(priv->mmio + PHYREG15);
+                val |= PHYREG15_CTLE_EN;
+                writel(val, priv->mmio + PHYREG15);
+
+		/* Set tx_rterm = 50 ohm and rx_rterm = 43.5 ohm */
+                val = PHYREG7_TX_RTERM_50OHM << PHYREG7_TX_RTERM_SHIFT;
+                val |= PHYREG7_RX_RTERM_44OHM << PHYREG7_RX_RTERM_SHIFT;
+                writel(val, priv->mmio + PHYREG7);
+
+		rockchip_combphy_param_write(priv->phy_grf, &cfg->con0_for_sata, true);
+		rockchip_combphy_param_write(priv->phy_grf, &cfg->con1_for_sata, true);
+		rockchip_combphy_param_write(priv->phy_grf, &cfg->con2_for_sata, true);
+		rockchip_combphy_param_write(priv->phy_grf, &cfg->con3_for_sata, true);
+		rockchip_combphy_param_write(priv->pipe_grf, &cfg->pipe_con0_for_sata, true);
+		rockchip_combphy_param_write(priv->pipe_grf, &cfg->pipe_con1_for_sata, true);
 		break;
 	case PHY_TYPE_SGMII:
 	case PHY_TYPE_QSGMII:
@@ -458,25 +582,35 @@ static int rk3588_combphy_cfg(struct rockchip_combphy_priv *priv)
 
 	/* 100MHz refclock signal is good */
 	clk_set_rate(&priv->ref_clk, 100000000);
-	param_write(priv->phy_grf, &cfg->pipe_clk_100m, true);
+	rockchip_combphy_param_write(priv->phy_grf, &cfg->pipe_clk_100m, true);
 	if (priv->mode == PHY_TYPE_PCIE) {
 		/* PLL KVCO tuning fine */
-		val = readl(priv->mmio + (0x20 << 2));
-		val &= ~GENMASK(4, 2);
-		val |= 0x4 << 2;
-		writel(val, priv->mmio + (0x20 << 2));
+		val = 4 << PHYREG33_PLL_KVCO_SHIFT;
+		rockchip_combphy_updatel(priv, PHYREG33_PLL_KVCO_MASK,
+					 val, PHYREG33);
+
+		/* Enable controlling random jitter. */
+		writel(PHYREG12_PLL_LPF_ADJ_VALUE, priv->mmio + PHYREG12);
 
 		/* Set up rx_trim: PLL LPF C1 85pf R1 1.25kohm */
-		val = 0x4c;
-		writel(val, priv->mmio + (0x1b << 2));
+		writel(PHYREG27_RX_TRIM_RK3588, priv->mmio + PHYREG27);
 
-		/* Set up su_trim: T3 */
-		val = 0xb0;
-		writel(val, priv->mmio + (0xa << 2));
-		val = 0x47;
-		writel(val, priv->mmio + (0xb << 2));
-		val = 0x57;
-		writel(val, priv->mmio + (0xd << 2));
+		/* Set up su_trim: */
+		writel(PHYREG11_SU_TRIM_0_7, priv->mmio + PHYREG11);
+
+		val = PHYREG13_RESISTER_HIGH_Z << PHYREG13_RESISTER_SHIFT;
+		val |= PHYREG13_CKRCV_AMP0;
+		rockchip_combphy_updatel(priv, PHYREG13_RESISTER_MASK, val, PHYREG13);
+
+		val = readl(priv->mmio + PHYREG14);
+		val |= PHYREG14_CKRCV_AMP1;
+		writel(val, priv->mmio + PHYREG14);
+	}
+
+	if (dev_read_bool(priv->dev, "rockchip,enable-ssc")) {
+		val = readl(priv->mmio + PHYREG8);
+		val |= PHYREG8_SSC_EN;
+		writel(val, priv->mmio + PHYREG8);
 	}
 
 	return 0;
@@ -501,12 +635,14 @@ static const struct rockchip_combphy_grfcfg rk3588_combphy_grfcfgs = {
 	.con2_for_pcie		= { 0x0008, 15, 0, 0x00, 0x0101 },
 	.con3_for_pcie		= { 0x000c, 15, 0, 0x00, 0x0200 },
 	.con0_for_sata		= { 0x0000, 15, 0, 0x00, 0x0129 },
-	.con1_for_sata		= { 0x0004, 15, 0, 0x00, 0x0040 },
+	.con1_for_sata		= { 0x0004, 15, 0, 0x00, 0x0000 },
 	.con2_for_sata		= { 0x0008, 15, 0, 0x00, 0x80c1 },
 	.con3_for_sata		= { 0x000c, 15, 0, 0x00, 0x0407 },
 	/* pipe-grf */
 	.pipe_con0_for_sata	= { 0x0000, 11, 5, 0x00, 0x22 },
 	.pipe_con1_for_sata	= { 0x0000, 2, 0, 0x00, 0x2 },
+	.pipe_pcie1l0_sel       = { 0x0100, 0, 0, 0x01, 0x0 },
+	.pipe_pcie1l1_sel       = { 0x0100, 1, 1, 0x01, 0x0 },
 };
 
 static const struct rockchip_combphy_cfg rk3588_combphy_cfgs = {
